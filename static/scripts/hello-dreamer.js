1baae06199648360fb596b3f9c9ff26817d92054
rubixqmusic <54426321+rubixqmusic@users.noreply.github.com>
2021-12-30 14:34:44 -0700

    this is almost ready for launch

--- static/scripts/hello-dreamer.js
+++ static/scripts/hello-dreamer.js
@@ -126,7 +126,7 @@ $(document).ready(function() {
   //back to main section
 
     $("#back-to-main-button").click(function() {
-        var url = "/";
+        var url = "/#random-stuff";
         window.open(url, '_self');
       
     });
